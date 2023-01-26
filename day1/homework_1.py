9c7aecf6fe3e47d7a70e22364e1dcd8795918018
alavidzenino <alavidzenino@gmail.com>
2023-01-27 03:02:45 +0400

    day2 lesson

---
+++ day1/homework_1.py
@@ -0,0 +1,11 @@
+name = "Nino"
+surname = "Alavidze"
+age = 35
+weight = 53.8
+occupation = "billing administrator"
+company = "Caucasus online"
+hobby = "snowboarding"
+
+print("I am " + name + " " + surname + ", my age is " + str(age) + ".\n" + "My weight is " + str(weight))
+print("I work at " + company + " as a " + occupation +"\n" + "On weekands i go out to the hills and " + hobby)
+print("I hope i have done my homework well :)")
---
+++ day1/lesson1.py
@@ -0,0 +1,13 @@
+name = "nino"
+
+surname = "alavidze"
+age = 22   # int (integer) tipis cvladi
+height = 156.8  #float tipis cvladi
+#boolean (bool) aris mcdari an swori,
+
+knows_programming = True #true an false
+is_ugly = False
+
+print(name + " "+ surname)
+#print(type(age)) #cvladis tipis gageba
+print(name + " " + str(age)) #win str dacerit int-s gardaqmnis stringad
